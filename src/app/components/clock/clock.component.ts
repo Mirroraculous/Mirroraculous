d3eb3f92751669f10b9c0e9d6922dce3ba237244
Justin <Pashweetie@gmail.com>
2019-09-11 13:45:55 -0600

    almost final

--- src/app/components/clock/clock.component.ts
+++ src/app/components/clock/clock.component.ts
@@ -9,8 +9,10 @@ import { Component, OnInit } from '@angular/core';
 export class ClockComponent implements OnInit {
   nowish;
   hours;
+  militaryHours;
   minutes;
   seconds;
+  showClockOptions = false;
   extension;
   military = false;
   timerId= null;
@@ -24,17 +26,15 @@ export class ClockComponent implements OnInit {
     this.nowish = new Date();
     this.minutes = this.leftPadZero(this.nowish.getMinutes());
     this.seconds = this.leftPadZero(this.nowish.getSeconds());
-    if(this.military){
-      this.hours = this.leftPadZero(this.nowish.getHours());
-    }else{
-      if (this.nowish.getHours()>12){
-        this.extension = "PM";
-      }
-      else{
-        this.extension = "AM";
-      }
-      this.hours = this.nowish.getHours()%12;
+    this.hours =  this.leftPadZero(this.nowish.getHours()%12);
+    this.militaryHours = this.leftPadZero(this.nowish.getHours());
+    if (this.nowish.getHours()>12){
+      this.extension = "PM";
     }
+    else{
+      this.extension = "AM";
+    }
+    
   }
   ngOnDestroy() {
     clearInterval(this.timerId);
@@ -43,6 +43,18 @@ export class ClockComponent implements OnInit {
     setInterval(() => {
       this.setTime();
     }, 1000);
+  }
+  private updateShow(val){
+    if(val=== 'clock'){
+      this.showClockOptions = !this.showClockOptions;
+    }
+  }
+  private clockChoice(val){
+    if(val === 'military'){
+      this.military =true;
+    }else{
+      this.military = false;
+    }
   }
   private leftPadZero(value: number) {
     return value < 10 ? `0${value}` : value.toString();
