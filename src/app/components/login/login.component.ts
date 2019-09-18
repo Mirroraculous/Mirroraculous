4aac9ea6280b674d08dadd3bac8d6eacc55ebb46
Justin <Pashweetie@gmail.com>
2019-09-18 16:29:37 -0600

    added comments and suggestions

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+  //import service here as well
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  //import service through the constructor
   constructor() { }
 
   ngOnInit() {
+  }
+  //gets called when user hits a submit key
+  aSubmittedDataFunction(){
+    // put a call here to a service that posts values to the backend. Enpoint is POST "localhost:3000/api/auth"
   }
 
 }
