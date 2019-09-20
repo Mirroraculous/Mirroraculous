fc0f1b11d2a9de2c46c9d91214701ca4013c4aaa
Calan <cfolger@msudenver.edu>
2019-09-20 10:06:52 -0600

    Lots of changes

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-  //import service here as well
+import { FormBuilder } from '@angular/forms';
+import { LoginService } from '../../Services/login.service';
+//import service here as well
 
 
 @Component({
@@ -8,16 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+
   // where you store form info and what you send to the backend
   DTO;
   //import service through the constructor
-  constructor() { }
+  constructor(private loginService: LoginService) { }
 
   ngOnInit() {
   }
   //gets called when user hits a submit key
   aSubmittedDataFunction(){
+    // this.loginService.checkUserPassCombo(this.DTO).subscribe(
+    //   val => {
+    //     if (val === 200){
+
+    //     }
+    //     else{
+    //     }
+    //   }
+    // );
     // put a call here to a service that posts values to the backend. Enpoint is POST "localhost:3000/api/auth"
   }
-
 }
---
+++ src/app/services/login.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+
+@Injectable({ providedIn: 'root' })
+export class LoginService {
+
+  private url = "localhost:3000/api/auth";  // URL to web api
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(
+    private http: HttpClient) { }
+
+
+  /** GET email and pass combo. Will 404 if id not found */
+  checkUserPassCombo(userPassCombo): Observable<any> {
+    return this.http.get<any>(this.url, userPassCombo).pipe(
+      tap(_ => this.log(`Attempting to log in ...`)),
+      catchError(this.handleError<any>(`userPassCombo`, userPassCombo))
+    );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+  }
+}
