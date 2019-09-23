6993a464eece7f609413da2f213f6951ff93e1ab
TimLucca <tim.lucca88@gmail.com>
2019-09-23 13:18:15 -0600

    changed directory path for ../../services/ so that case sensitive machines dont yell at you

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import { LoginService } from '../../Services/login.service';
+import { LoginService } from '../../services/login.service';
 //import service here as well
 interface DTO{
   email: string;
