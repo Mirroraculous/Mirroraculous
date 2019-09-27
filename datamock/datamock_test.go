8d5556f4ee91d7445ed2020536f98a0b2e4111db
Tim Lucca <tim.lucca88@gmail.com>
2019-09-27 01:40:35 -0600

    moved reoccuring strings to globals

--- datamock/datamock_test.go
+++ datamock/datamock_test.go
@@ -1,50 +1,76 @@
 package datamock
 
 import (
+	"fmt"
 	"testing"
 )
 
+var (
+	name   string
+	email  string
+	pwd    string
+	date   string
+	time1  string
+	time2  string
+	event1 string
+	event2 string
+	id     string
+	status int
+)
+
+func init() {
+	name = "Tim"
+	email = "tim@tim.tim"
+	pwd = "123"
+	date = "12/01/2000"
+	time1 = "9:00AM"
+	time2 = "9:00PM"
+	event1 = "do things"
+	event2 = "abcd"
+}
+
 func TestAddUser(t *testing.T) {
-	id, status := AddUser("Tim", "tim@tim.tim", "123")
+	id, status = AddUser(name, email, pwd)
 	if status != 200 {
 		t.Errorf("Test failed, expected status '%d', got: '%d'", 200, status)
 	} else if len(id) < 1 {
 		t.Errorf("Test failed, id not properly created")
 	}
 
-	_, status = AddUser("Tim", "tim@tim.tim", "123")
+	_, status = AddUser(name, email, pwd)
 	if status != 400 {
 		t.Errorf("Test failed, duplicate registration expected status '%d', got: '%d'", 400, status)
 	}
 }
 
 func TestLoginUser(t *testing.T) {
-	id, status := LoginUser("tim@tim.tim", "123")
-	if status != 200 {
-		t.Errorf("Test failed, return error: '%s'", id)
-	}
-
-	id, status = LoginUser("tim@tim.ti", "123")
+	id, status = LoginUser("tim@tim.ti", pwd)
 	if id != "Username not found" {
 		t.Errorf("Test failed, expected Username not found, got '%s'", id)
 	} else if status != 401 {
 		t.Errorf("Test failed, expected status 401, got '%d'", status)
 	}
 
-	id, status = LoginUser("tim@tim.tim", "13")
+	id, status = LoginUser(email, "13")
 	if id != "Invalid Password" {
 		t.Errorf("Test failed, expected Username not found, got '%s'", id)
 	} else if status != 401 {
 		t.Errorf("Test failed, expected status 401, got '%d'", status)
 	}
+
+	id, status = LoginUser(email, pwd)
+	if status != 200 {
+		t.Errorf("Test failed, return error: '%s'", id)
+	}
+	fmt.Println(id)
 }
 
 func TestGetUser(t *testing.T) {
+	fmt.Println(id)
 	user, status := GetUser("")
 	if status == 200 {
 		t.Errorf("Test failed, expected status 400, got status '%d'", status)
 	}
-	id, _ := LoginUser("tim@tim.tim", "123")
 	user, status = GetUser(id)
 	if status != 200 {
 		t.Errorf("Test failed, expected no error, got status '%d'", status)
@@ -54,39 +80,37 @@ func TestGetUser(t *testing.T) {
 }
 
 func TestAddEvent(t *testing.T) {
-	id, _ := LoginUser("tim@tim.tim", "123")
-	e := AddEvent(id, "12/01/2000", "nine:00AM", "do things")
+	e := AddEvent(id, date, "nine:00AM", event1)
 	if e.Error() != "Invalid time" {
 		t.Errorf("Test failed, expected Invalid time, got '%s'", e.Error())
 	}
-	e = AddEvent(id, "12/01/22", "9:00AM", "do things")
+	e = AddEvent(id, "12/01/22", time1, event1)
 	if e.Error() != "Invalid date" {
 		t.Errorf("Test failed, expected Invalid date, got '%s'", e.Error())
 	}
-	e = AddEvent(id, "12/01/2000", "9:00PM", "do things")
+	e = AddEvent(id, date, time2, event1)
 	if e != nil {
 		t.Errorf("Test failed, expected no error, got '%s'", e.Error())
 	}
-	e = AddEvent(id, "12/01/2000", "9:00PM", "abcd")
+	e = AddEvent(id, date, time2, event2)
 	if e.Error() != "Event exists for that time" {
 		t.Errorf("Test failed, expected Event exists for that time, got %s", e.Error())
 	}
-	e = AddEvent(id, "12/01/2000", "9:00AM", "abcd")
+	e = AddEvent(id, date, time1, event2)
 	if e != nil {
 		t.Errorf("Test failed, expected no error, got %s", e.Error())
 	}
-	e = AddEvent(id, "12/01/2000", "9:00PM", "abcd")
+	e = AddEvent(id, date, time2, event2)
 	if e.Error() != "Event exists for that time" {
 		t.Errorf("Test failed, expected Event exists for that time, got %s", e.Error())
 	}
-	e = AddEvent("", "12/01/2000", "9:00PM", "abcd")
+	e = AddEvent("", date, time2, event2)
 	if e.Error() != "User doesn't exist" {
 		t.Errorf("Test failed, expected User doesn't exist, got %s", e.Error())
 	}
 }
 
 func TestGetCalendar(t *testing.T) {
-	id, _ := LoginUser("tim@tim.tim", "123")
 	calendar, status := GetCalendar(id)
 	if status != 200 {
 		t.Errorf("Expected status code 200, got '%d'", status)
@@ -102,7 +126,6 @@ func TestGetCalendar(t *testing.T) {
 }
 
 func TestUpdateEvent(t *testing.T) {
-	id, _ := LoginUser("tim@tim.tim", "123")
 	calendar, _ := GetCalendar(id)
 	status := UpdateEvent(id, calendar[0].Event[0].ID, "8:00PM", "eat")
 	if status != 200 {
@@ -125,7 +148,6 @@ func TestUpdateEvent(t *testing.T) {
 }
 
 func TestDeleteEvent(t *testing.T) {
-	id, _ := LoginUser("tim@tim.tim", "123")
 	calendar, _ := GetCalendar(id)
 	status := DeleteEvent(id, calendar[0].Event[0].ID)
 	if status != 200 {
