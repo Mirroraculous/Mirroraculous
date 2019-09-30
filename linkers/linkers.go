822212c299778d5397ab8d6ad06927e73a71ae53
Tim Lucca <tim.lucca88@gmail.com>
2019-09-30 01:33:36 -0600

    checking for existing email works

--- linkers/linkers.go
+++ linkers/linkers.go
@@ -6,13 +6,41 @@ import (
 
 	"github.com/mirroraculous/mirroraculous/config"
 	"github.com/mirroraculous/mirroraculous/models"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
-func AddUser(newUser models.User) (string, int) {
-	found, _ := config.User.Find(context.Background(), newUser.Email)
+// func AddUser(newUser models.User) (string, int) {
+// 	return addUser(newUser, func(s string) interface{} {
+// 		val, _ := config.User.Find({email: s})
+// 		return val
+// 	})
+// }
 
-	if found != nil {
-		return "User already exists", 400
+// func addUser(newUser models.User, find func(email string) interface{}) (string, int) {
+// 	found := find(newUser.Email)
+
+// 	if found != nil {
+// 		return "User already exists", 400
+// 	}
+
+// 	id, e := config.User.InsertOne(context.Background(), newUser)
+
+// 	if e != nil {
+// 		return e.Error(), 500
+// 	}
+
+// 	return fmt.Sprintf("%v", id.InsertedID), 200
+// }
+
+func AddUser(newUser models.User) (string, int) {
+	var u models.User
+	err := config.User.FindOne(context.Background(), bson.D{{"email", newUser.Email}}).Decode(&u)
+	if u.Email == newUser.Email {
+		return "Email already in use", 400
+	}
+
+	if err != nil {
+		return "Server error", 500
 	}
 
 	id, e := config.User.InsertOne(context.Background(), newUser)
