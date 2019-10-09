4c36b50fcd0317132b37fa493b40b1df257ebf5a
Justin <Pashweetie@gmail.com>
2019-10-09 13:45:52 -0600

    clean up clean up everybody do their share

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -4,58 +4,45 @@ import { LoginService } from '../../services/login.service';
 import { SessionService } from '../../services/session.service';
 import { Router } from "@angular/router";
 
-
-//import service here as well
 interface DTO{
   email: string;
   password: string;
 }
-
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent implements OnInit {
   message = '';
   DTO: DTO={
     email: "",
     password: "",
   }
-
-  // where you store form info and what you send to the backend
-  //import service through the constructor
   constructor(
     private router: Router,
     private session: SessionService,
-    private loginService: LoginService) { 
-    
+    private loginService: LoginService) {     
   }
 
   ngOnInit() {
-    // this.checkSession();
-    // this.session.checkSession();
-
   }
-  //gets called when user hits a submit key
+
   aSubmittedDataFunction(){
-    // console.log(this.DTO);
     this.loginService.checkUserPassCombo(this.DTO).subscribe(
       val => {
         console.log(val.status);
         if (val.status === 200 || val.status === 204){
           this.message = '';
-          // console.log(val.body);
           localStorage.setItem('sessionToken',val.body);
           this.router.navigate(['/home']);
         }
         else{
           this.message = 'Failed to login. Incorrect credentials';
-          // console.log(val);
         }
       }
     );
-    //put a call here to a service that posts values to the backend. Enpoint is POST "localhost:3000/api/auth"
   }
 }
