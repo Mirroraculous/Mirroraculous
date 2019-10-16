a02a48544f4455eecbf5c41eec702a68643e9e65
Pashweetie <Pashweetie@gmail.com>
2019-10-15 23:40:31 -0600

    small fix

--- src/app/components/events/events.component.ts
+++ src/app/components/events/events.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl } from '@angular/forms';
 import { Time } from '@angular/common';
 import { EventsService } from 'src/app/services/events.service';
-import { SessionService } from '../../services/session.service';
+import { SessionService } from '../../auth/session.service';
 
 
 interface DTO{
