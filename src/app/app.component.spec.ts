617f465ba80f3cc792244b382713eb4e302efbe4
Justin <Pashweetie@gmail.com>
2019-10-28 12:44:48 -0600

    made updoots to make things less obnoxious

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
-import { BrowserModule, By } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TokenInterceptor } from './auth/token.interceptor';
+
 import { AppRoutingModule } from './app-routing.module';
-// import { AppComponent } from './app.component';
+import { AppComponent } from './app.component';
 import { OverlayComponent } from './components/overlay/overlay.component';
 // import { OverlayModule } from '@angular/cdk/overlay';
 import { MatDialogModule } from '@angular/material/dialog';
@@ -16,9 +18,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './components/home/home.component';
 import { ClockComponent } from './components/clock/clock.component';
 
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { LoginComponent } from './components/login/login.component';
 import { HttpClientModule } from '@angular/common/http';
+import { EventsComponent } from './components/addEvents/addEvents.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { JwtModule } from "@auth0/angular-jwt";
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { DeleteEventComponent } from './components/delete-event/delete-event.component';
+import { UpdateEventComponent } from './components/update-event/update-event.component';
+import { EventComponent } from './components/event/event.component';
+
+
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -29,7 +41,13 @@ describe('AppComponent', () => {
         HomeComponent,
         ClockComponent,
         LoginComponent,
-        RegisterPageComponent
+        RegisterPageComponent,
+        EventsComponent,
+        PageNotFoundComponent,
+        CalendarComponent,
+        DeleteEventComponent,
+        UpdateEventComponent,
+        EventComponent,
       ],
       imports: [
         BrowserModule,
@@ -38,7 +56,24 @@ describe('AppComponent', () => {
         MatDialogModule,
         BrowserAnimationsModule,
         MatIconModule,
+        ReactiveFormsModule,
         HttpClientModule,
+        JwtModule.forRoot({
+          config: {
+            tokenGetter: () => {
+              return localStorage.getItem("access_token");
+            },
+            whitelistedDomains: ["example.com"],
+            blacklistedRoutes: ["example.com/examplebadroute/"],
+          }
+        })
+      ],
+      providers: [JwtHelperService,
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: TokenInterceptor,
+          multi: true
+        }
       ],
     }).compileComponents();
   }));
@@ -54,7 +89,13 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Mirroraculous');
   });
-
+  it(`should have a register sub page`,()=>{
+    const fixture = TestBed.createComponent(RegisterPageComponent);
+    const app = fixture.debugElement.componentInstance
+    expect(app.DTO.name).toBe("");
+    expect(app.DTO.name).toBe("");
+    expect(app.DTO.name).toBe("");
+  });
   // it('should render title', () => {
   //   const fixture = TestBed.createComponent(AppComponent);
   //   fixture.detectChanges();
