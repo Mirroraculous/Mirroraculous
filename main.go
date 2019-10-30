bc2c88cef56576a76db5ef47ca8f169d3eca2cf2
Tim Lucca <tim.lucca88@gmail.com>
2019-10-29 23:04:48 -0600

    google api connection mostly working

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/mirroraculous/mirroraculous/config"
 	"github.com/mirroraculous/mirroraculous/handlers"
+	"github.com/mirroraculous/mirroraculous/oauth"
 )
 
 func main() {
@@ -16,6 +17,12 @@ func main() {
 		fmt.Println("DB unable to connect")
 		return
 	}
+
+	if e != nil {
+		fmt.Println("Unable to get credentials")
+		return
+	}
+
 	server := gin.Default()
 	server.Use(cors.New(cors.Config{
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
@@ -35,6 +42,8 @@ func main() {
 	server.POST("/api/calendar", handlers.AddEvent)
 	server.PUT("/api/calendar/:id", handlers.UpdateEvent)
 	server.DELETE("/api/calendar/:id", handlers.DeleteEvent)
+
+	server.GET("/api/googlelogin", oauth.GoogleLogin)
 
 	err := server.Run(":3000")
 	if err != nil {
---
+++ oauth/oauth.go
@@ -0,0 +1,84 @@
+package oauth
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/gin-gonic/gin"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+type Credentials struct {
+	Cid     string   `json:"client_id"`
+	Csecret string   `json:"client_secret"`
+	Redir   []string `json:"redirect_uris"`
+	Origins []string `json:"javascript_origins"`
+}
+
+func Cred() (Credentials, oauth2.Config, error) {
+	var c Credentials
+	var conf oauth2.Config
+	f, e := ioutil.ReadFile("oauth/client_id.json")
+	if e != nil {
+		fmt.Println(e.Error())
+		return c, conf, e
+	}
+	json.Unmarshal(f, &c)
+	conf = oauth2.Config{
+		ClientID:     c.Cid,
+		ClientSecret: c.Csecret,
+		RedirectURL:  c.Redir[1],
+		Scopes: []string{
+			"https://www.googleapis.com/auth/calendar.events.readonly",
+		},
+		Endpoint: google.Endpoint,
+	}
+	fmt.Println(conf.RedirectURL)
+	return c, conf, e
+}
+
+func getLoginURL(state string) string {
+	_, conf, _ := Cred()
+	fmt.Println(conf.RedirectURL)
+	return conf.AuthCodeURL(state)
+}
+
+func randToken() string {
+	b := make([]byte, 32)
+	rand.Read(b)
+	return base64.StdEncoding.EncodeToString(b)
+}
+
+func GoogleLogin(c *gin.Context) {
+	state := randToken()
+	c.JSON(200, getLoginURL(state))
+}
+
+func GoogleAuth(c *gin.Context) {
+	_, conf, e := Cred()
+	if e != nil {
+		c.JSON(500, "Server credentials failed")
+		return
+	}
+	token, e := conf.Exchange(oauth2.NoContext, c.Query("code"))
+	if e != nil {
+		c.JSON(401, "Unauthorized")
+		return
+	}
+
+	client := conf.Client(oauth2.NoContext, token)
+	email, e := client.Get("https://www.googleapis.com/oauth2/v3/userinfo")
+	if e != nil {
+		c.JSON(401, "Email unverified")
+		return
+	}
+
+	defer email.Body.Close()
+	data, _ := ioutil.ReadAll(email.Body)
+	fmt.Printf("Email: %s\n", string(data))
+	c.Status(200)
+}
