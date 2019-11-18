26076ad598b64d9ba2817e6ebcc4a1a613be0213
Pashweetie <Pashweetie@gmail.com>
2019-11-17 21:34:00 -0700

    updoots for tim

--- src/app/components/alarm/alarm/alarm.component.ts
+++ src/app/components/alarm/alarm/alarm.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularWaitBarrier } from 'blocking-proxy/built/lib/angular_wait_barrier';
 interface Alarm{
   isActive: boolean;
   time: number;
@@ -12,11 +13,60 @@ export class AlarmComponent implements OnInit {
   isMinified = false;
   alarms: Alarm[] = [];
   currentAdding = false;
+  nowish;
+  hours;
+  militaryHours;
+  minutes;
+  seconds;
+  showClockOptions = false;
+  extension;
+  military = false;
+  timerId= null;
+  current_alarm_string;
   constructor() { }
 
   ngOnInit() {
+    this.setTime();    
+    this.timerId = this.updateTime();
   }
-  onSubmit(){
+  private setTime(){          
+    this.nowish = new Date();
+    this.minutes = this.leftPadZero(this.nowish.getMinutes());
+    this.seconds = this.leftPadZero(this.nowish.getSeconds());    
+    this.militaryHours = this.nowish.getHours();
+    // console.log(this.militaryHours);
+    if((this.militaryHours%12===0 && this.militaryHours>=12)||(this.militaryHours%12===0 && this.militaryHours===0)){
+      this.hours = this.militaryHours%12 +12;
+    }else{      
+      this.hours =  this.militaryHours%12;
+    }
+    if (this.militaryHours>=12){
+      this.extension = "PM";
+    }
+    else{
+      this.extension = "AM";
+    }
+    this.current_alarm_string.append(this.hours+":"+this.minutes+" "+this.extension);
+    this.current_alarm_string.append(this.hours+":"+this.minutes+this.extension);
+    this.alarms.forEach(element => {
+      if(element){
+        this.current_alarm_string.forEach(elements =>{
+          if(element == elements){
+            this.makeNoise();
+          }
+        });
+      }
+    });
+    
+  }
+  makeNoise(){
+
+  }
+  ngOnDestroy() {
+    clearInterval(this.timerId);
+  }
+  onSubmit(val){
+    console.log("HEY LOOK AT THIS CONSOLE.LOG",val);
     //add backend call
   }
   changeAdding(){
@@ -24,5 +74,14 @@ export class AlarmComponent implements OnInit {
   }
   changeView (){
     this.isMinified = !this.isMinified;
+  }
+  
+  private updateTime() {
+    setInterval(() => {
+      this.setTime();
+    }, 1000);
+  }
+  private leftPadZero(value: number) {
+    return value < 10 ? `0${value}` : value.toString();
   }
 }
