b11fd8ff52788491d6d7861a688ce79411a7ee26
Pashweetie <Pashweetie@gmail.com>
2019-11-17 20:25:08 -0700

    first commit woohoo

--- src/app/components/alarm/alarm/alarm.component.ts
+++ src/app/components/alarm/alarm/alarm.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+interface Alarm{
+  isActive: boolean;
+  time: number;
+}
 @Component({
   selector: 'app-alarm',
   templateUrl: './alarm.component.html',
   styleUrls: ['./alarm.component.scss']
 })
 export class AlarmComponent implements OnInit {
-
+  isMinified = false;
+  alarms: Alarm[] = [];
+  currentAdding = false;
   constructor() { }
 
   ngOnInit() {
   }
-
+  changeAdding(){
+    this.currentAdding = !this.currentAdding;
+  }
+  changeView (){
+    this.isMinified = !this.isMinified;
+  }
 }
